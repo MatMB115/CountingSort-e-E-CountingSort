d452490bee56c5f7cd766cefa3513e8f73c84b24
Matheus Martins <matheusmbatista2009@gmail.com>
2022-05-03 13:56:39 -0300

    update

--- E-Counting.c
+++ E-Counting.c
@@ -1,49 +1,45 @@
 //EM DESENVOLVIMENTO!!!
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void countingSort(int array[], int size) {
-    int output[1000];
+void countingSort(int arrA[], int *arrB, int tam){
 
-    // Find the largest element of the array
-    int max = array[0];
-    for (int i = 1; i < size; i++) {
-        if (array[i] > max)
-        max = array[i];
+    // Encontrar o maior elemento do vetor
+    int max = arrA[0];
+    for (int i = 1; i < tam; i++) {
+        if (arrA[i] > max)
+        max = arrA[i];
     }
 
-    // The size of count must be at least (max+1) but
-    // we cannot declare it as int count(max+1) in C as
-    // it does not support dynamic memory allocation.
-    // So, its size is provided statically.
-    int count[max];
+    //Alocar um vetor para armazenar a frequência
+    int* count = (int*) calloc(tam, sizeof(int));
 
-    // Initialize count array with all zeros or use calloc or use memset 0
-    for (int i = 0; i <= max; ++i) {
+    // Inicializar o vetor com 0 ou calloc ou memset 0
+    /*for (int i = 0; i <= max; ++i) {
         count[i] = 0;
-    }
+    }*/
 
     // Store the count of each element
-    for (int i = 0; i < size; i++) {
-        count[array[i]]++;
-    }
-    int j = 0;
-    for(int i = 0; i < max;){
-        while(count[i] > 0){
-            array[j] = i;
-            j++;
-            count[i]--;
-        }
+    for (int i = 0; i < tam; i++) {
+        count[arrA[i]]++;
     }
 
-    // Copy the sorted elements into original array
-    for (int i = 0; i < size; i++) {
-        array[i] = output[i];
+    // Store the cummulative count of each array
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    // Find the index of each element of the original array in count array, and
+    // place the elements in output array
+    for (int i = tam - 1; i >= 0; i--) {
+        arrB[count[arrA[i]] - 1] = arrA[i];
+        count[arrA[i]]--;
     }
 }
 
 // Function to print an array
-void printArray(int array[], int size) {
+void printArray(int* array, int size) {
   for (int i = 0; i < size; ++i) {
     printf("%d  ", array[i]);
   }
@@ -52,8 +48,9 @@ void printArray(int array[], int size) {
 
 // Driver code
 int main() {
-  int array[] = {4,8,7,9,8,5,2,1,2,8,7,4,1,2,5,4,8,9,3,1,4,7,2,1,4,2,3,23,56,98,784,54,21,654,77,124,785,354,147};
-  int n = sizeof(array) / sizeof(array[0]);
-  countingSort(array, n);
-  printArray(array, n);
+  int arrA[] = {4,8,7,9,8,5,2,1,2,8,7,4,1,2,5,4,8,9,3,1,4,7,2,1,4,2,3,23,56,98,784,54,21,654,77,124,785,354,147};
+  int n = sizeof(arrA) / sizeof(arrA[0]);
+  int *arrB = (int*) malloc(n * sizeof(int));
+  countingSort(arrA, arrB, n);
+  printArray(arrB, n);
 }
