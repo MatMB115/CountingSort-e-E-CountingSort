9d6c2fac9d1a215e3498fab5681ba380d0e3e250
Matheus Martins <matheusmbatista2009@gmail.com>
2022-05-04 21:58:55 -0300

    Tá vivo
    Ambos os algoritmos funcionam em um range de 1 à k elementos (k inteiro positivo).

--- Counting.c
+++ Counting.c
@@ -3,17 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void countingSort(int *arrA, int *arrB, int tam){
+void countingSort(int *arrA, int *arrB, int max, int tam){
     int i;
-    // Encontrar o maior elemento do vetor
-    int max = arrA[0];
-    for (int i = 1; i < tam; i++) {
-        if (arrA[i] > max)
-        max = arrA[i];
-    }
-
     //Alocar um vetor para armazenar a frequência
-    int* count = calloc(max, sizeof(int));
+    int* count = calloc(max + 1, sizeof(int));
 
     // Inicializar o vetor com 0 ou calloc ou memset 0
     /*for (int i = 0; i < max; ++i) {
@@ -23,19 +16,19 @@ void countingSort(int *arrA, int *arrB, int tam){
 
     // Guardar a frequências dos elementos no auxiliar Count
     for (i = 0; i < tam; i++) {
-        count[arrA[i]-1]++;
+        count[arrA[i]]++;
     }
 
     // Particularidade do CountingSort Original
     // Guardar o acumulativo das valores menores que um elemento i do vetor original (A)
-    for (i = 1; i < max; i++) {
+    for (i = 1; i <= max; i++) {
         count[i] += count[i - 1];
     }
 
     // Percorrer o vetor original com início no último elemento, subtituindo os índices nos elementos do vetor count e decrescendo a cada atribuição
     for (i = tam - 1; i >= 0; i--) {
-        arrB[count[arrA[i] - 1] - 1] = arrA[i];
-        count[arrA[i]-1]--;
+        arrB[count[arrA[i]] - 1] = arrA[i];
+        count[arrA[i]]--;
     }
 }
 
@@ -62,12 +55,20 @@ void isOrdered(int *arr, int tam){
 }
 
 int main() {
-  int arrA[] = {4,8,7,9,8,5,2,1,2,8,7,4,1,2,5,4,8,9,3,1,4,7,2,1,4,2,3,23,56,98,784,54,21,654,77,124,785,354,147};
-  int n = sizeof(arrA) / sizeof(arrA[0]);
-  int *arrB = (int*) malloc(n * sizeof(int));
-  countingSort(arrA, arrB, n);
-  isOrdered(arrB, n);
-  imprimeArr(arrB, n);
-  isOrdered(arrA, n);
-  imprimeArr(arrA, n);
+  int arrA[] = {4,8,7,9,8,5,2,1,2,8,7,4,1,2,5,4,24,785,354,147,74,21,654,77,124,78,785,354,147};
+
+  //Calcular o tamanho do vetor
+  //Encontrar maior elemento do vetor
+  int tam = sizeof(arrA) / sizeof(arrA[0]);
+  int max = arrA[0];
+  for (int i = 1; i < tam; i++) {
+      if (arrA[i] > max)
+      max = arrA[i];
+  }
+  int *arrB = (int*) malloc(tam * sizeof(int));
+  countingSort(arrA, arrB, max, tam);
+  isOrdered(arrB, tam);
+  imprimeArr(arrB, tam);
+  isOrdered(arrA, tam);
+  imprimeArr(arrA, tam);
 }
--- E-Counting.c
+++ E-Counting.c
@@ -1,38 +1,32 @@
-//Não funcional
+//FUNCIONAL
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void eCountingSort(int *arrA, int *arrB, int tam){
+void countingSort(int *arrA, int *arrB, int max, int tam){
     int i, j = 0;
-    // Encontrar o maior elemento do vetor
-    int max = arrA[0];
-    for (int i = 1; i < tam; i++) {
-        if (arrA[i] > max)
-        max = arrA[i];
-    }
+    //Alocar um vetor para armazenar a frequência
+    int* count = calloc(max + 1, sizeof(int));
 
-    // Alocar um vetor para armazenar a frequência com tamanho do maior elemento encontrado
-    int count[max];
-
-    // Inicializar os elementos do vetor com 0
-    for (i = 0; i <= max; ++i) {
+    // Inicializar o vetor com 0 ou calloc ou memset 0
+    /*for (int i = 0; i < max; ++i) {
         count[i] = 0;
     }
-    /* ou memset(count, 0, sizeof(count));*/
+    ou memset(count, 0, sizeof(count));*/
 
     // Guardar a frequências dos elementos no auxiliar Count
     for (i = 0; i < tam; i++) {
-        count[arrA[i]-1]++;
+        count[arrA[i]]++;
     }
 
-    //E-Counting particularidade
-    for(i = 0; i < tam; i++){
-        while(count[i] > 0){
-            arrB[j] = i;
-            j++;
-            count[i]--;
-        }
+    // Particularidade do Efficient CountingSort
+    // Aproveitar a frequência dos elementos e o índice do vetor count para preencher o vetor B ordenado
+    for (i = 0; i <= max; i++){
+      while(count[i] > 0){
+        arrB[j] = i;
+        j++;
+        count[i]--;
+      }
     }
 }
 
@@ -45,11 +39,34 @@ void imprimeArr(int* array, int size){
 }
 
 
+//Função para verificar o sucesso da ordenação
+void isOrdered(int *arr, int tam){
+  int i, flag = 0, aux = arr[0];
+
+  for(i=1;i<tam;i++){
+    if(arr[i] < aux){
+      flag = 1;
+    }
+  }
+  if(flag == 0){printf("\nVetor Ordenado\n");}
+  else{printf("\nVetor Desordenado\n");}
+}
+
 int main() {
-  int arrA[] = {4,8,7,9,8,5,2,1,2,8,7,4,1,2,5,4,8,9,3,1,4,7,2,1,4,2,3,23,56,98,784,54,21,654,77,124,785,354,147};
-  int n = sizeof(arrA) / sizeof(arrA[0]);
-  int *arrB = (int*) malloc(n * sizeof(int));
-  eCountingSort(arrA, arrB, n);
-  imprimeArr(arrB, n);
-  imprimeArr(arrA, n);
+  int arrA[] = {4,8,7,9,8,5,2,1,2,8,7,4,1,2,5,4,24,785,354,147,74,21,654,77,784,124,78,785,354,147};
+
+  //Calcular o tamanho do vetor
+  //Encontrar maior elemento do vetor
+  int tam = sizeof(arrA) / sizeof(arrA[0]);
+  int max = arrA[0];
+  for (int i = 1; i < tam; i++) {
+      if (arrA[i] > max)
+      max = arrA[i];
+  }
+  int *arrB = (int*) malloc(tam * sizeof(int));
+  countingSort(arrA, arrB, max, tam);
+  isOrdered(arrB, tam);
+  imprimeArr(arrB, tam);
+  isOrdered(arrA, tam);
+  imprimeArr(arrA, tam);
 }
